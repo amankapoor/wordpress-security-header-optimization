3fc0e6c47da350e121b72c8c2fd2bf1fd3ea594f
o10n-x <info@optimization.team>
2018-06-09 12:53:09 +0200

    core update

--- security-header-optimization.php
+++ security-header-optimization.php
@@ -12,7 +12,7 @@ namespace O10n;
  * @wordpress-plugin
  * Plugin Name:       Security Header Optimization
  * Description:       Advanced security header optimization toolkit. Content-Security-Policy, Strict Transport Security (HSTS), Public-Key-Pins (HPKP), X-XSS-Protection and CORS.
- * Version:           0.0.52
+ * Version:           0.0.53
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
  * GitHub Plugin URI: https://github.com/o10n-x/wordpress-security-header-optimization
@@ -30,7 +30,7 @@ if (defined('WP_INSTALLING') && WP_INSTALLING) {
 }
 
 // settings
-$module_version = '0.0.52';
+$module_version = '0.0.53';
 $minimum_core_version = '0.0.44';
 $plugin_path = dirname(__FILE__);
 
